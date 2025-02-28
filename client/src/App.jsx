bc990c4716a195af3de2ec501284f3393afd0ef8
fiba fathima <fibaaminnu@gmail.com>
2025-02-28 09:43:35 +0530

    completed the assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './components/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ExcuseCard from './components/excuseCard'
+import AddExcuseForm from './components/AddExcuseForm'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/excuses" element={<ExcuseCard />} />
+        <Route path='/add-excuse' element={<AddExcuseForm/>} />
       </Routes>
     </Router>
   )
